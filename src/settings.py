410e05a22a5fcf95843fa939569dcd2a4fc6e948
janolockhart <janolockhart@gmail.com>
2025-09-26 19:05:10 -0300

    Added debug varible

--- src/settings.py
+++ src/settings.py
@@ -7,6 +7,8 @@ IAM_PATH = os.getenv("IAM_PATH")
 CVL_PATH = os.getenv("CVL_PATH")
 RIMES_PATH = os.getenv("RIMES_PATH")
 
-BATCH_SIZE = int(os.getenv("BATCH_SIZE")) if not os.getenv("DEBUG_MODE") else int(os.getenv("DEBUG_BATCH_SIZE"))
-TRAIN_SPLIT = float(os.getenv("TRAIN_SPLIT")) if not os.getenv("DEBUG_MODE") else float(os.getenv("DEBUG_TRAIN_SPLIT"))
-VAL_SPLIT = float(os.getenv("VAL_SPLIT")) if not os.getenv("DEBUG_MODE") else float(os.getenv("DEBUG_VAL_SPLIT"))
+DEBUG_MODE = bool(os.getenv("DEBUG_MODE"))
+
+BATCH_SIZE = int(os.getenv("BATCH_SIZE")) if not DEBUG_MODE else int(os.getenv("DEBUG_BATCH_SIZE"))
+TRAIN_SPLIT = float(os.getenv("TRAIN_SPLIT")) if not DEBUG_MODE else float(os.getenv("DEBUG_TRAIN_SPLIT"))
+VAL_SPLIT = float(os.getenv("VAL_SPLIT")) if not DEBUG_MODE else float(os.getenv("DEBUG_VAL_SPLIT"))
