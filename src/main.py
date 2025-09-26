87f44274250c78f61c49ff8afa552a6effc7bc0e
janolockhart <janolockhart@gmail.com>
2025-09-26 15:58:12 -0300

    Extracted model into its own file

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ import numpy as np
 from keras import layers
 from dataloader import IAMLineDataloader
 
+from model import build_model
+
 def main():
     input_shape = (32, 256, 1)
 
@@ -77,40 +79,7 @@ def main():
         print(label.numpy())
 
     # MODEL
-    model = keras.Sequential([
-        keras.Input(shape=(input_shape)),
-        layers.Conv2D(filters=32, kernel_size=5, padding="same"),
-        layers.BatchNormalization(),
-        layers.ReLU(),
-        layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="valid"),
-
-        layers.Conv2D(filters=64, kernel_size=5, padding="same"),
-        layers.BatchNormalization(),
-        layers.ReLU(),
-        layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="valid"),
-        
-        layers.Conv2D(filters=128, kernel_size=3, padding="same"),
-        layers.BatchNormalization(),
-        layers.ReLU(),
-        layers.MaxPool2D(pool_size=(2, 1), strides=(2, 1), padding="valid"),
-        
-        layers.Conv2D(filters=128, kernel_size=3, padding="same"),
-        layers.BatchNormalization(),
-        layers.ReLU(),
-        layers.MaxPool2D(pool_size=(2, 1), strides=(2, 1), padding="valid"),
-        
-        layers.Conv2D(filters=256, kernel_size=3, padding="same"),
-        layers.BatchNormalization(),
-        layers.ReLU(),
-        layers.MaxPool2D(pool_size=(2, 1), strides=(2, 1), padding="valid"),
-        
-        
-        layers.Reshape((64, 256)),
-        
-        layers.Bidirectional(layers.LSTM(256, return_sequences=True), merge_mode="concat"),
-        layers.Bidirectional(layers.LSTM(256, return_sequences=True), merge_mode="concat"),
-        layers.Dense(len(unique_chars) + 1, activation=None),
-    ])
+    model = build_model(input_shape, unique_chars)
 
     model.summary()
 
