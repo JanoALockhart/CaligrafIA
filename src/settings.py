4efc6af5d2d1884522ec9e564b2d64887f496ca0
janolockhart <janolockhart@gmail.com>
2025-09-28 16:15:40 -0300

    Added epochs to env variables

--- src/settings.py
+++ src/settings.py
@@ -9,6 +9,7 @@ RIMES_PATH = os.getenv("RIMES_PATH")
 
 DEBUG_MODE = bool(os.getenv("DEBUG_MODE"))
 
+EPOCHS = int(os.getenv("EPOCHS")) if not DEBUG_MODE else int(os.getenv("DEBUG_EPOCHS"))
 BATCH_SIZE = int(os.getenv("BATCH_SIZE")) if not DEBUG_MODE else int(os.getenv("DEBUG_BATCH_SIZE"))
 TRAIN_SPLIT = float(os.getenv("TRAIN_SPLIT")) if not DEBUG_MODE else float(os.getenv("DEBUG_TRAIN_SPLIT"))
 VAL_SPLIT = float(os.getenv("VAL_SPLIT")) if not DEBUG_MODE else float(os.getenv("DEBUG_VAL_SPLIT"))
