e26f0a4fe989b798174b336c0da35cb3e67c7252
Jano Axel Lockhart <janolockhart@gmail.com>
2025-10-17 00:32:08 +0000

    DatasetBuilder class

---
+++ src/datasets/dataset_builder.py
@@ -0,0 +1,16 @@
+from abc import ABC
+from abc import abstractmethod
+
+class DatasetBuilder(ABC):
+
+    @abstractmethod
+    def get_training_set(self):
+        pass
+
+    @abstractmethod
+    def get_validation_set(self):
+        pass
+
+    @abstractmethod
+    def get_test_set(self):
+        pass
