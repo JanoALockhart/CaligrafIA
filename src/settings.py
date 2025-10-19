72e94a435224b92b83b54752dfbc12420d350f1e
Jano Axel Lockhart <janolockhart@gmail.com>
2025-10-19 03:51:28 +0000

    Added image height and width to .env

--- src/settings.py
+++ src/settings.py
@@ -14,6 +14,9 @@ BATCH_SIZE = int(os.getenv("BATCH_SIZE")) if not DEBUG_MODE else int(os.getenv("
 TRAIN_SPLIT = float(os.getenv("TRAIN_SPLIT")) if not DEBUG_MODE else float(os.getenv("DEBUG_TRAIN_SPLIT"))
 VAL_SPLIT = float(os.getenv("VAL_SPLIT")) if not DEBUG_MODE else float(os.getenv("DEBUG_VAL_SPLIT"))
 
+IMG_HEIGHT = int(os.getenv("IMG_HEIGHT"))
+IMG_WIDTH = int(os.getenv("IMG_WIDTH"))
+
 EAGER_EXECUTION = False if not DEBUG_MODE else bool(os.getenv("EAGER_EXECUTION"))
 
 TEST_IMG_PATH = os.getenv("TEST_IMG_PATH")
